9ee0f7ab1cb87376de00a2ca8563a65a1469acfe
Yash708262 <yash708262@gmail.com>
2025-11-09 11:40:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
-  // your code here
+  // your code here s = s.trimStart();
+
+  // If the string is empty, return it directly
+  if (s === "") return "";
+
+  // Find the index of the first space
+  const spaceIndex = s.indexOf(" ");
+
+  // If there is no space, return the whole string
+  if (spaceIndex === -1) return s;
+
+  // Otherwise, return everything before the first space
+  return s.slice(0, spaceIndex);
 }
 
 // Do not change the code below
